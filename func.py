a8c447595b25231f39cd3ad8b8ba2e6a3607610f
Daniel Corin <dcorin6@gmail.com>
2015-01-30 21:50:45 -0800

    make 'where' more concise, add find_where, reject, every, some

--- func.py
+++ func.py
@@ -1,67 +1,85 @@
-# perform _d[key] = value
+''' Perform _d[key] = value '''
 def assoc(_d, key, value):
 	from copy import deepcopy
 	d = deepcopy(_d)
 	d[key] = value
 	return d
 
-# sequentially apply a list of functions to data
+''' Sequentially apply a list of functions to data '''
 def pipeline_each(data, fns):
 	return reduce(lambda a,x: map(x, a), fns, data)
 
-# apply fn to key before assoc
+''' Apply fn to key before assoc '''
 def call(fn, key):
 	def apply_fn(record):
 		return assoc(record, key, fn(record.get(key)))
 	return apply_fn
 
+''' Get first element of list or None '''
 def first(data):
-	return data[0]
+	return data[0] if data else None
 
+''' Get all but last element of list '''
 def initial(data):
 	return data[:-1]
 
+''' Get last element of list or None '''
 def last(data):
-	return data[-1]
+	return data[-1] if data else None
 
+''' Get all but first element of list '''
 def rest(data):
 	return data[1:]
 
 # Let's recreate Underscore.js in Python
 
+''' Apply fn to all elements in data '''
 def each(data, fn):
 	map(lambda x: fn(x), data)
 
 # Python already has map and reduce :)
 
-def find(predicate, data):
-	return first(filter(predicate, data))
+''' Return first item in data that satisfies fn '''
+def find(fn, data):
+	return first(filter(fn, data))
 
-# check if d contains key-value pair prop
-# a blank key value pair is contained by all d
-def contains_prop(d, prop):
-	if not prop.keys():
-		return True
-	key = first(prop.keys())
-	return d.get(key) == prop.get(key)
-
-# a blank property list is contained by all d
-# check d for each property
-# if any are not in d, return false
+''' 
+	Check if d contains props
+	A blank property list is contained by all d
+	Check that props is a subset of d
+'''
 def contains_props(d, props):
 	if not props:
 		return True
-	elif not contains_prop(d, first(props)):
-		return False
-	else:
-		return contains_props(d, rest(props))
+	return set(props.items()).issubset(set(d.items()))
 
+''' Return all dicts in data take contain all properties '''
 def where(data, properties):
-	if type(properties) != type(list()):
-		properties = [properties]
 	return filter(lambda d: contains_props(d, properties), data)
 
-# create a list of an attribute for a list of dicts  
+''' Find first dict that contains properties '''
+def find_where(data, properties):
+	return first(where(data, properties))
+
+''' Return items that do not satisfy fn '''
+def reject(fn, data):
+	return filter(lambda x: not fn(x), data)
+
+'''
+Return True is fn of every item is True
+No items => True
+'''
+def every(fn, data):
+	return reduce(lambda a,x: a and fn(x), data, True)
+
+'''
+Return True is fn of any item is True
+No items => False
+'''
+def some(fn, data):
+	return reduce(lambda a,x: a or fn(x), data, False)
+
+''' Create a list of an attribute for a list of dicts '''
 def pluck(data, key):
 	return map(lambda x: x.get(key), data)
 
--- test_func.py
+++ test_func.py
@@ -17,7 +17,9 @@ class TestFuncFunctions(unittest.TestCase):
 			'name': 'Madeon',
 			'country': 'France',
 			'active': True
-		},
+		},{
+			'name': 'Sparce'
+		}
 	]
 
 	def test_assoc(self):
@@ -56,56 +58,103 @@ class TestFuncFunctions(unittest.TestCase):
 		self.assertEqual(cmp(arr, self.bands), 0)
 
 	def test_first(self):
+		# get first element
 		self.assertEqual(3, first([3,4,5,6]))
+		# empty list returns None
+		self.assertEqual(None, first([]))
 
 	def test_initial(self):
+		# get all but last value
 		self.assertEqual([3,4,5], initial([3,4,5,6]))
+		# empty list returns empty list
+		self.assertEqual([], initial([]))
 
 	def test_last(self):
+		# get list item
 		self.assertEqual(6, last([3,4,5,6]))
+		# empty list returns None
+		self.assertEqual(None, last([]))
 
 	def test_rest(self):
+		# get all but first value
 		self.assertEqual([4,5,6], rest([3,4,5,6]))
+		# empty list returns empty list
+		self.assertEqual([], rest([]))
 
 	def test_find(self):
 		arr = [2,3,4,5,6,7,8]
-
+		# first item divisible by 4
 		self.assertEqual(4, find(lambda x: x%4==0, arr))
-
+		# first item greater than 7
 		self.assertEqual(7, find(lambda x: x>6, arr))
 
 
-	def test_contains_prop(self):
-		d = {'active':True, 'prop2':'hello'}
-		self.assertTrue(contains_prop(d, {'active':True}))
-		self.assertTrue(contains_prop(d, {'prop2':'hello'}))
-
-		self.assertFalse(contains_prop(d, {'active':False}))
-		self.assertFalse(contains_prop(d, {'prop3':'testing'}))
-
 	def test_where(self):
-		bands_where = where(self.bands, [{'active': True}])
+		# get all active bands
+		bands_where = where(self.bands, {'active': True})
 		bands_where_for = [b for b in self.bands if b.get('active')]
 		self.assertEqual(cmp(bands_where, bands_where_for), 0)
 
-		bands_where = where(self.bands, [{'active': False}])
-		bands_where_for = [b for b in self.bands if not b.get('active')]
-		self.assertEqual(cmp(bands_where, bands_where_for), 0)
-
+		# get all inactive bands
 		bands_where = where(self.bands, {'active': False})
-		bands_where_for = [b for b in self.bands if not b.get('active')]
+		bands_where_for = [b for b in self.bands if b.get('active') == False]
 		self.assertEqual(cmp(bands_where, bands_where_for), 0)
 
-		bands_where2 = where(self.bands, [{'active': True}, {'country':'France'}])
+		# get all active bands from France
+		bands_where2 = where(self.bands, {'active': True, 'country': 'France'})
 		bands_where_for2 = [b for b in self.bands if b.get('active') and b.get('country') == 'France']
 		self.assertEqual(cmp(bands_where2, bands_where_for2), 0)
 
+		# get all bands
 		bands_where = where(self.bands, {})
 		self.assertEqual(cmp(bands_where, self.bands), 0)
 
+	def test_find_where(self):
+		# get first inactive band
+		bands_where = find_where(self.bands, {'active': False})
+		bands_where_for = first([b for b in self.bands if b.get('active') == False])
+		self.assertEqual(bands_where, bands_where_for)
+
+		# get first active band from France
+		bands_where = find_where(self.bands, {'active': True, 'country': 'France'})
+		bands_where_for = first([b for b in self.bands if b.get('active') and b.get('country') == 'France'])
+		self.assertEqual(bands_where, bands_where_for)
+
+		# try to find some other property
+		bands_where = find_where(self.bands, {'monster': True})
+		bands_where_for = first([b for b in self.bands if b.get('monster')])
+		self.assertEqual(bands_where, bands_where_for)
+		self.assertEqual(bands_where, None)
+
+	def test_reject(self):
+		# reject numbers that are even
+		ls = [1,2,3,4,5,6,7]
+		odds = [l for l in ls if l%2==1]
+		reject_odds = reject(lambda x: x%2==0, ls)
+		self.assertEqual(odds, reject_odds)
+
+	def test_every(self):
+		self.assertFalse(every(lambda x: x, [True, True, False]))
+		self.assertTrue(every(lambda x: x, [True, True]))
+		# are all numbers even
+		self.assertTrue(every(lambda x: x%2==0, [2, 10, 200]))
+		# every x in [] is true
+		self.assertTrue(every(lambda x: x, []))
+
+	def test_some(self):
+		# some number is even
+		self.assertTrue(some(lambda x: x%2==0, [1,3,5,7,8]))
+		# no number is a multiple of 5
+		self.assertFalse(some(lambda x: x%5==0, [1,3,7,8]))
+		# no element is even
+		self.assertFalse(some(lambda x: x%2==0, []))
+
 	def test_pluck(self):
+		# get list of bands' countries
 		countries = pluck(self.bands, 'country')
-		self.assertEqual(countries, [b['country'] for b in self.bands])
+		self.assertEqual(countries, [b.get('country') for b in self.bands])
 
 if __name__ == '__main__':
 	unittest.main()
+
+
